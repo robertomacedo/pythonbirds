859e385eaed188e3c9ab5b45580fce38e5e3ece3
Roberto <macedorss20@gmail.com>
2020-10-19 16:41:41 -0300

    Implementada a direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -5,9 +5,18 @@ class Pessoa:
         self.nome = nome
         self.filhos = list(filhos)
 
-
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributo_de_classe(cls):
+        return f'{cls} -olhos {cls.olhos}'
+
+
 if __name__ == '__main__':
     roberto = Pessoa(nome='Roberto Macedo Santos')
     antonio = Pessoa(roberto, nome='Antonio')
@@ -24,3 +33,6 @@ if __name__ == '__main__':
     print(roberto.__dict__)
     print(Pessoa.olhos)
     print(roberto.olhos)
+    print(id(Pessoa.olhos), id(roberto.olhos), id(antonio.olhos))
+    print(Pessoa.metodo_estatico(), antonio.metodo_estatico())
+    print(Pessoa.nome_e_atributo_de_classe(), antonio.nome_e_atributo_de_classe())
